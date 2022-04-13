e999567c5c8586f775a9747afb2ce61aa8135798
JMG24a <jmg24a@gmail.com>
2022-04-13 16:12:10 -0400

    .

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 // import { useRouter } from 'next/router'
 import { DetailProduct } from '@components/DetailProducts'
 import { GetStaticProps} from 'next'
